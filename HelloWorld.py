0a21825936b12f83957950cc54247c4e0ea44329
lingchong <lingchong2016@gmail.com>
2017-07-11 21:36:08 +0800

    Initial commit

---
+++ HelloWorld.py
@@ -0,0 +1,16 @@
+from numpy import *
+
+print "Hello World, Python And Machine Learning!"
+
+arrayRand = random.rand(4, 4)
+print arrayRand
+
+matRand = mat(arrayRand)
+print matRand
+
+invMatRand = matRand.I
+print invMatRand
+
+print matRand * invMatRand
+
+print matRand * invMatRand - eye(4)
