0ebae0740e062b203150adc6c5d97c9ff616bc0a
lingchong <lingchong2016@gmail.com>
2017-08-17 22:03:45 +0800

    添加yield语句实现for...in代码。

--- hello_world.py
+++ hello_world.py
@@ -46,35 +46,76 @@ import time
 #     print w
 
 
-def enumerate_it(sequence, start=0):
+def enumerate_it(_sequence, _start=0):
 
     """
         yield语句仅用于定义生成器函数时在其函数体内使用。当yield在一个函数定义中使用时，该函数不再被视为普通函数，而被编译器看作生成器函
     数使用。调用生成器函数可以返回生成器函数的迭代器对象iterator，调用iterator.next()会重复返回一个yield语句后面跟随的表达式（即：
     yield expression_list，返回的是expression_list），直到迭代完毕或出现异常。
-        在返回expression_list的同时，生成器函数会执行到下一个yield语句为止，保留（即冻结）所有生成器函数执行的信息，等待下一次的next()
+        在返回expression_list的同时，生成器函数会停止执行yield的下一条语句，保留（冻结）所有生成器函数执行的信息，等待下一次的next()
     调用。
         从Python 2.5开始，yield语句支持try...finally语法结构，finally里面的语句会在迭代器函数中的资源会回收（引用计数为0、GC或调用
     iterator.close()函数）时执行。
     """
 
     try:
-        n = start
-        for elem in sequence:
+        n = _start
+        for elem in _sequence:
             yield n, elem
             n += 1
     finally:
         print 'Execute finally clause.'
 
 
-seasons = ['Spring', 'Summer', 'Fall', 'Winter']
-g = enumerate_it(seasons)
-print g.next()
-print g.next()
-print g.next()
+# seasons = ['Spring', 'Summer', 'Fall', 'Winter']
+# g = enumerate_it(seasons)
+# print g.next()
+# print g.next()
+# print g.next()
+#
+# for i in range(10):
+#     print datetime.now()
+#     time.sleep(1)
+#
+# g.close()
 
-for i in range(10):
-    print datetime.now()
-    time.sleep(1)
+def print_num(_num):
+    print _num
 
-g.close()
+
+def for_iterator_imitation(_list_index):
+    i = 0
+    n = len(_list_index)
+
+    while i < n:
+        yield _list_index[i]
+        i += 1
+
+
+def for_clause_imitation():
+
+    """
+        模拟for...in语法结构。
+        注意：当for_iterator_imitation(...)函数执行完毕（即其中的while语句跳出）时，iterator.next()函数会返回（触发）一个
+    StopIteration异常，通过捕获该异常可以判断迭代完成。
+    """
+
+    i = 3
+    list_index = []
+    counter = 0
+    while counter < i:
+        list_index.append(counter)
+        counter += 1
+
+    print list_index
+
+    iterator = for_iterator_imitation(list_index)
+    while True:
+        try:
+            j = iterator.next()
+            print j
+        except StopIteration:
+            break
+
+
+for_clause_imitation()
